bbc88cec52377bc6e7e97ed71a2c98e57643df48
fux <jan.dick@bdp.org>
2026-07-10 04:39:17 +0200

    Adds script for creating players and their diaries

---
+++ scripts/create_players.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+import json
+import os
+from typing import Any, Dict, List
+
+TXTAD_PATH = "/srv/txtad-data/"
+# TXTAD_PATH = "/home/fux/homepage/test/"
+DOST_PATH = "/usr/bin/dost/homepage/"
+
+PATH_TO_TXTAD_CHARS = Path(TXTAD_PATH, "dost/game_files/Characters/")
+PATH_TO_TXTAD_PLAYERS = Path(TXTAD_PATH, "dost/game_files/Players/")
+PATH_TO_TXTAD_DIARIES = Path(TXTAD_PATH, "dost/game_files/Diaries/")
+PATH_TO_DOST_PLAYERS = Path(DOST_PATH, "data/")
+
+JSON_CTX_TEMPLATE = {
+    "attributes": {
+    },
+    "description": {
+        "logic": "",
+        "one_time_events": "",
+        "permanent_events": "",
+        "shared": False,
+        "txt": "."
+    },
+    "id": "",
+    "listeners": [],
+    "name": "",
+    "permeable": True,
+    "priority": 0,
+    "re_entrycondition": "",
+    "shared": True
+}
+
+def get_character(player_key, distribution) -> str: 
+    chars = [c["character"] for c in distribution if c["player"] == player_key]
+    if len(chars) != 1: 
+        exit(f"too many or no chars for player: {player_key} => {chars}")
+
+    with open(PATH_TO_TXTAD_CHARS.joinpath(f"{chars[0]}.ctx"), 'r') as f:
+        return json.load(f)
+
+def transform(player, distribution): 
+    player_key = player["key"]
+    char = get_character(player_key, distribution)
+
+    # Create Player
+    player_ctx = JSON_CTX_TEMPLATE.copy()
+    player_ctx["id"] = f"Players/{player_key}"
+    player_ctx["name"] = player['name']
+    player_ctx["attributes"]["key"] = player_key
+    player_ctx["attributes"]["cur_char"] = char["attributes"]["key"]
+
+    # Create Diary
+    diary_ctx = JSON_CTX_TEMPLATE.copy()
+    diary_ctx["id"] = f"Diaries/{player_key}"
+    diary_ctx["name"] = f"{player['name']}'s Diary"
+    diary_ctx["attributes"]["key"] = player_key
+    diary_ctx["attributes"]["player"] = player_key
+    diary_ctx["attributes"]["cur_char"] = char["attributes"]["key"]
+    diary_ctx["attributes"]["cur_username"] = char["attributes"]["username"]
+
+
+    print("PLAYER: ", json.dump(player_ctx))
+    print("DIARY: ", json.dump(diary_ctx))
+    inp = input("Safe? (y/n): ")
+    if inp != "y":
+        exit("aborted.")
+
+    # Safe Player
+    with open(PATH_TO_TXTAD_PLAYERS.joinpath(f"{player_key}.ctx"), 'w') as f:
+        json.dump(player_ctx, f)
+
+    # Safe Diary
+    with open(PATH_TO_TXTAD_DIARIES.joinpath(f"{player_key}.ctx"), 'w') as f:
+        json.dump(diary_ctx, f)
+   
+    # Also update and store char: 
+    char["attributes"]["player"] = player_key
+    with open(PATH_TO_TXTAD_CHARS.joinpath(f"{chars[0]}.ctx"), 'w') as f:
+        json.dump(char, f)
+    
+
+if __name__ == "__main__": 
+    with open("resources/character_zuteilung.json", 'r') as f:
+        distribution = json.load(f)
+    players = []
+    for file in PATH_TO_DOST_PLAYERS.glob("*.json"):
+        with open(file, "r") as f:
+            players.append(transform(json.load(f), distribution))
